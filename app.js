35db0ed11a650b66b86a25398e166ed757d8205e
Ivan <ivan.jaimes.y90m@statefarm.com>
2020-09-05 01:08:21 -0500

    set morgan logger to save to ../morgan.log in production mode

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ let tasksRouter = require("./routes/tasks");
 
 var app = express();
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+if (app.get("env") == "production") {
+  app.use(
+    morgan("common", {
+      skip: function (req, res) {
+        return res.statusCode < 400;
+      },
+      stream: __dirname + "../morgan.log",
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+}
 
 app.use("/", indexRouter);
 app.use("/patients", patientsRouter);
